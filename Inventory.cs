170b98c344b8a7b3c9bf764cc9df331eb3660009
zSakuraEvilz <nguyenduy.sakuraevil@gmail.com>
2016-08-01 23:54:58 +0700

    PokeBag
    Init Project

---
+++ Inventory.cs
@@ -0,0 +1,86 @@
+﻿using PokemonGo.RocketAPI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using POGOProtos.Data;
+using POGOProtos.Data.Player;
+using POGOProtos.Enums;
+using POGOProtos.Inventory;
+using POGOProtos.Inventory.Item;
+using POGOProtos.Settings.Master;
+using System;
+
+namespace PokeBag
+{
+    class Inventory
+    {
+        private readonly Client _client;
+        public Inventory(Client client)
+        {
+            _client = client;
+
+        }
+
+        public async Task<IEnumerable<PokemonData>> GetPokemons()
+        {
+            try
+            {
+                var inventory = await _client.Inventory.GetInventory();
+                return inventory.InventoryDelta.InventoryItems.Select(i => i.InventoryItemData?.PokemonData).Where(p => p != null && p?.PokemonId > 0);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+           
+        }
+
+        public async Task<int[]> GetRequiredCandy(List<PokemonSettings> pokemonSettings, Candy[] pokemonFamilies, PokemonId pokemon)
+        {
+            try
+            {
+                var settings = pokemonSettings.Single(x => x.PokemonId == pokemon);
+                var familyCandy = pokemonFamilies.Single(x => settings.FamilyId == x.FamilyId);
+                return new[] { familyCandy.Candy_, settings.CandyToEvolve };
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+           
+        }
+
+        public async Task<IEnumerable<Candy>> GetPokemonFamilies()
+        {
+            try
+            {
+                var inventory = await _client.Inventory.GetInventory();
+                return
+                    inventory.InventoryDelta.InventoryItems.Select(i => i.InventoryItemData?.Candy)
+                        .Where(p => p != null && p?.FamilyId != PokemonFamilyId.FamilyUnset);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+           
+        }
+
+        public async Task<IEnumerable<PokemonSettings>> GetPokemonSettings()
+        {
+            try
+            {
+                var templates = await _client.Download.GetItemTemplates();
+                return
+                    templates.ItemTemplates.Select(i => i.PokemonSettings)
+                        .Where(p => p != null && p?.FamilyId != PokemonFamilyId.FamilyUnset);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            
+        }
+
+    }
+}
---
+++ Logic.cs
@@ -0,0 +1,91 @@
+﻿using PokemonGo.RocketAPI;
+using PokemonGo.RocketAPI.Enums;
+using System.Threading.Tasks;
+using System;
+using POGOProtos.Data;
+using System.Collections.Generic;
+using POGOProtos.Networking.Responses;
+
+namespace PokeBag
+{
+    class Logic
+    {
+        public readonly Client _client;
+        public readonly ISettings _clientSettings;
+        public readonly Inventory _inventory;
+
+        public bool isLogin = false;
+        public Random rand = new Random();
+
+        public Logic(ISettings clientSettings)
+        {
+
+            _clientSettings = clientSettings;
+            _client = new Client(_clientSettings);
+            _inventory = new Inventory(_client);
+
+        }
+
+        public async Task<bool> Login()
+        {
+            try
+            {
+                if (_clientSettings.AuthType == AuthType.Ptc)
+                    await _client.Login.DoPtcLogin(_clientSettings.PtcUsername, _clientSettings.PtcPassword);
+                else if (_clientSettings.AuthType == AuthType.Google)
+                {
+                    await _client.Login.DoGoogleLogin(_clientSettings.GoogleUsername, _clientSettings.GooglePassword);
+                }
+                isLogin = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                isLogin = false;
+                return false;
+            }
+           
+        }
+
+        public async Task<bool> TransferPokemon(ulong pokemonId)
+        {
+            try
+            {
+                await _client.Inventory.TransferPokemon(pokemonId);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+
+        public async Task<PokemonData> EvolvePokemon(ulong pokemonId)
+        {
+            try
+            {
+                var inventory = await _client.Inventory.EvolvePokemon(pokemonId);
+                return inventory.EvolvedPokemonData;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+           
+        }
+
+        public async Task<string> GetPlayerName()
+        {
+            try
+            {
+                var player = await _client.Player.GetPlayer();
+                return player.PlayerData.Username;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
